db210046a64931173fa1c5d467f0083864523223
arjec111 <arjecdizon99@gmail.com>
2026-03-23 13:54:48 +0800

    fix vercel scripts

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,9 +1,7 @@
-cat > vercel_build.sh << 'EOF'
 #!/bin/bash
 set -e
 export FLUTTER_ALLOW_ROOT=true
 
-# Clone Flutter if not present
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git --branch 3.29.2 --single-branch --depth 1
 fi
@@ -13,4 +11,3 @@ flutter --version
 flutter config --enable-web
 flutter pub get
 flutter build web --release --no-tree-shake-icons
-EOF
--- vercel_install.sh
+++ vercel_install.sh
@@ -1,15 +1,11 @@
-cat > vercel_install.sh << 'EOF'
 #!/bin/bash
 export FLUTTER_ALLOW_ROOT=true
 
-if cd flutter; then
-  git pull && cd ..
-else
+if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git --branch 3.29.2 --single-branch --depth 1
 fi
 
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
 flutter --version
 flutter config --enable-web
 flutter clean
-EOF
