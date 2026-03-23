cb9c592b0b147de67a1c7355b1daf41c49e6cdb0
arjec111 <arjecdizon99@gmail.com>
2026-03-23 13:00:08 +0800

    vercel deploy

---
+++ vercel_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if cd flutter; then
+  git pull && cd ..
+else
+  git clone https://github.com/flutter/flutter.git
+fi
+flutter/bin/flutter config --enable-web
+flutter/bin/flutter clean
