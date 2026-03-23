b5cae8f70e990322e3b3a3a92e680631a22933e2
arjec111 <arjecdizon99@gmail.com>
2026-03-23 13:28:23 +0800

    v

--- vercel_install.sh
+++ vercel_install.sh
@@ -4,7 +4,7 @@ export FLUTTER_ALLOW_ROOT=true
 if cd flutter; then
   git pull && cd ..
 else
-  git clone https://github.com/flutter/flutter.git -b 3.27.4 --depth 1
+  git clone https://github.com/flutter/flutter.git -b 3.29.2 --depth 1
 fi
 
 export PATH="$PATH:`pwd`/flutter/bin"
