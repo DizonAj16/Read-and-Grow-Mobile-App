93c4c1d704bb69d57dd8b71567089719f0b6c5b9
arjec111 <arjecdizon99@gmail.com>
2026-03-23 13:42:05 +0800

    v

--- vercel_install.sh
+++ vercel_install.sh
@@ -1,12 +1,15 @@
+cat > vercel_install.sh << 'EOF'
 #!/bin/bash
 export FLUTTER_ALLOW_ROOT=true
 
 if cd flutter; then
   git pull && cd ..
 else
-  git clone https://github.com/flutter/flutter.git -b 3.29.2 --depth 1
+  git clone https://github.com/flutter/flutter.git --branch 3.29.2 --single-branch --depth 1
 fi
 
 export PATH="$PATH:`pwd`/flutter/bin"
+flutter --version
 flutter config --enable-web
 flutter clean
+EOF
